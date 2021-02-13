b5a044a12421bed4740a840b2fc64a2bd5a26a78
masonsteeger94 <masonsteeger@gmail.com>
2021-02-13 15:25:42 -0600

    added scripts for deployment

--- anichievements/src/containers/Username/Username.js
+++ anichievements/src/containers/Username/Username.js
@@ -49,11 +49,11 @@ const Username = (props) => {
             {
                 data ? props.setUser(data.User): 
                 <div>
-                  <h2>Enter Your AniList Username below!</h2>
+                  <h2>Enter Your AniList Username below:</h2>
                   <form>
                     <input type='text' value={input} onChange={(e) => onChangeHandler(e)}/>
                     <br/>
-                    <button type='submit' onClick={() =>onSubmitHandler()}>Log In!</button>
+                    <button type='submit' onClick={() =>onSubmitHandler()}>GET ACHIEVEMENTS!</button>
                   </form>
                   <br/>
                   <br/>
@@ -66,7 +66,7 @@ const Username = (props) => {
                   <div className='explanation'>
                     <h2>Anichievements is a project by Mason Steeger inspired by MALGraph that is an extenstion of your AniList profile. Unfortunately since MAL has restricted the use of their API, the achievements I wished to complete on MALGraph are no longer up to date, so I decided to make an achievement list myself!</h2>
                     <h2>In order to use this website you need to have an AniList profile (which you can create <a target="_blank" rel="noreferrer" href='https://anilist.co/signup'>here</a>) and some anime in your completed list! Once you have that, just type your username in the input box and you'll see your achievements!</h2>
-                    <h2>Any questions, concerns, or suggestions can be addressed to <a href="mailto:masonsteeger@gmail.com">Mason Steeger</a> via this email with the subject reading "Anichievements".</h2>
+                    <h2>Any questions, comments, concerns, or suggestions can be addressed to <a href="mailto:masonsteeger@gmail.com">Mason Steeger</a> via this email with the subject reading "Anichievements".</h2>
                     <h2>Happy Watching!</h2>
                   </div>
                 </div>
